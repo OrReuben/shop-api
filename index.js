67979b01c08344b46b7e55d75622e4ba8a302210
OrReuben <orreuven1243@gmail.com>
2022-10-22 17:12:40 +0300

    need to fix this

--- index.js
+++ index.js
@@ -3,29 +3,30 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
-const userRoute = require('./routes/user')
-const authRoute = require('./routes/auth')
-const productRoute = require('./routes/product')
-const cartRoute = require('./routes/cart')
-const orderRoute = require('./routes/order')
-const stripeRoute = require('./routes/stripe')
-const cors = require('cors')
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
+const stripeRoute = require("./routes/stripe");
+const cors = require("cors");
 
 mongoose
-  .connect(process.env.MONGO_URL)
+  .connect(
+    "mongodb+srv://Admin:NewPassword@cluster0.kuf9z1r.mongodb.net/Shop?retryWrites=true&w=majority"
+  )
   .then(() => console.log("DB connection successful"))
   .catch((err) => console.log(err));
 
+app.use(express.json());
 
-app.use(express.json())
-
-app.use(cors())
-app.use('/api/auth', authRoute)
-app.use('/api/users', userRoute)
-app.use('/api/products', productRoute)
-app.use('/api/carts', cartRoute)
-app.use('/api/orders', orderRoute)
-app.use('/api/checkout', stripeRoute)
+app.use(cors());
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
+app.use("/api/checkout", stripeRoute);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("backend server is running");
