71fdb9996798a84b006912f54b55764d4d27bfe5
OrReuben <orreuven1243@gmail.com>
2022-11-02 15:57:09 +0200

    bad search

--- index.js
+++ index.js
@@ -11,13 +11,13 @@ const orderRoute = require("./routes/order");
 const stripeRoute = require("./routes/stripe");
 const cors = require("cors");
 
-app.use(cors());
 
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => console.log("DB connection successful"))
   .catch((err) => console.log(err));
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoute);
